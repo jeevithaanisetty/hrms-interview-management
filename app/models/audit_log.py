6fdd928ef8d76b39fe8f8085d99a19453803071e
Peddi Raju Kattula <peddiraju176073@gmail.com>
2026-03-05 10:51:36 +0530

    Addressed PR review comments for audit logs model

--- app/models/audit_log.py
+++ app/models/audit_log.py
@@ -1,21 +1,21 @@
-from sqlalchemy import Column,String,DateTime,Text,ForeignKey 
-from sqlalchemy.dialects.postgresql import UUID,JSONB 
-from sqlalchemy.orm import relationship 
-from db.database import Base 
-from datetime import datetime 
-import uuid 
+from sqlalchemy import Column,String,DateTime,Text,ForeignKey
+from sqlalchemy.dialects.postgresql import UUID,JSONB
+# from sqlalchemy.orm import relationship
+from db.database import Base
+from sqlalchemy.sql import func
+import uuid
 
 
 class AuditLog(Base):
-    
+
     __tablename__="audit_logs"
-    
+
     id = Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
-    user_id = Column(UUID,nullable=False)   #who performed action 
-    entity_type = Column(String,nullable=False) 
-    entity_id = Column(UUID,nullable=False)
-    action = Column(String,nullable=False)    
+    user_id = Column(UUID(as_uuid=True),nullable=False)
+    entity_type = Column(String,nullable=False)
+    entity_id = Column(UUID(as_uuid=True),nullable=False)
+    action = Column(String,nullable=False)
     old_values = Column(JSONB,nullable=True)
     new_values = Column(JSONB,nullable=True)
-    description = Column(Text,nullable=True) 
-    created_at = Column(DateTime,default=datetime.utcnow)
+    description = Column(Text,nullable=True)
+    created_at = Column(DateTime(timezone=True),server_default=func.now())
