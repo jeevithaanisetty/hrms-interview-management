e27b7b5b11a82f9ed4c10ebd6d6aebe14705246c
jeevithaanisetty <jeevitha.anisetty@thezennialpro.com>
2026-03-16 00:16:48 +0530

    Modified config to use .env and added to .gitignore

--- app/config.py
+++ app/config.py
@@ -1,11 +1,10 @@
-import os
 from dotenv import load_dotenv
 #from typing import Optional
 from pydantic_settings import BaseSettings
 
 load_dotenv()
 class Settings(BaseSettings):
-    DATABASE_URL = os.getenv("DATABASE_URL")
+    DATABASE_URL:str
     SMTP_HOST:str="smtp.gmail.com"
     SMTP_PORT:int=587
     SMTP_USERNAME:str
--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from app.config import DATABASE_URL
+from app.config import Settings
  
-engine = create_engine(DATABASE_URL)
+engine = create_engine(Settings.DATABASE_URL)
  
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
  
